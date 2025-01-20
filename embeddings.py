636abc3b7cd1a10024c2cc303aace27cd00c4647
Ajoshi005 <116526886+Ajoshi005@users.noreply.github.com>
2025-01-20 05:59:10 +0000

    removed memory

--- embeddings.py
+++ embeddings.py
@@ -6,7 +6,7 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate,PromptTemplate
-from langchain.memory import ConversationBufferMemory
+# from langchain.memory import ConversationBufferMemory
 from langchain.globals import set_debug
 from dotenv import load_dotenv
 import streamlit as st
@@ -19,17 +19,16 @@ def query_llm(query):
     template = """You're a friendly, empathetic, and knowledgeable customer relationship manager assisting HSBC customers with their queries about migrating their accounts to RBC Bank. Your responses should be based on the information provided in the context, ensuring accuracy and relevance. When answering questions, provide clear and helpful responses that align with the RBC product Migration guide, without explicitly referencing the knowledge base. Instead, weave in the information naturally to enhance customer understanding. 
 
 {context}
-Below is the history of conversation before this question :{chat_history}
 
 Question: {question}
 
 Detailed Answer:"""
-    custom_rag_prompt = PromptTemplate.from_template(template)
+    custom_rag_prompt = ChatPromptTemplate.from_template(template)
     load_dotenv()
     index_name = 'rbchsbc-retrieval-augmentation'
     embeddings = OpenAIEmbeddings(openai_api_key=os.getenv('OPENAI_KEY'))
-    memory = ConversationBufferMemory(memory_key="chat_history")
-    llm = ChatOpenAI(model_name="gpt-4o-mini",memory=memory , prompt= custom_rag_prompt, temperature=0, openai_api_key=os.getenv("OPENAI_KEY"))
+#     memory = ConversationBufferMemory(memory_key="chat_history")
+    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0, openai_api_key=os.getenv("OPENAI_KEY"))
 
     PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]   # os.getenv("PINECONE_API_KEY")
 
@@ -42,7 +41,7 @@ Detailed Answer:"""
 
     rag_chain = (
             {"context": retriever, "question": RunnablePassthrough()}
-        #     | custom_rag_prompt
+            | custom_rag_prompt
             | llm
             | StrOutputParser()
     )
